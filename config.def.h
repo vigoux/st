39aec8d128d71a7656700c3057e44812d1e615e7
Thomas Vigouroux <tomvig38@gmail.com>
2019-12-27 11:47:08 +0100

    Use monospace font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "IBMPlexMono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
