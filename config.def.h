a9165c13bfc2bb8182e0d25cebb1a1b5f421a6b4
Thomas Vigouroux <tomvig38@gmail.com>
2020-04-26 19:03:03 +0200

    feat: alpha and adjustable size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=15:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=30:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -83,6 +83,9 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
+
+/* Opacity */
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
